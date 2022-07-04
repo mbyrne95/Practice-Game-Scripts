64b59296e035186c05a9b072e57c493f55cb7acf
mbyrne95 <76924518+mbyrne95@users.noreply.github.com>
2022-07-04 01:14:57 -0400

    added ne ranged enemy script, adjusted enemy scripts to better target players

--- Enemy/BasicRangedEnemy.cs
+++ Enemy/BasicRangedEnemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class BasicEnemyAI : MonoBehaviour, IEnemy 
+public class BasicRangedEnemy : MonoBehaviour, IEnemy
 {
     public GameObject xp;
 
@@ -13,9 +13,16 @@ public class BasicEnemyAI : MonoBehaviour, IEnemy
     public float attackRadius;
     public bool shouldRotate;
 
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    private float lastShootTime = 0;
+    [SerializeField]
+    private float fireRate;
+
+
     public LayerMask whatIsPlayer;
 
-    [HideInInspector]
+    
     public GameObject player;
 
     private Transform target;
@@ -28,17 +35,21 @@ public class BasicEnemyAI : MonoBehaviour, IEnemy
 
     private Collider2D col;
 
-    //private bool isInAttackRange;
+    private bool isInAttackRange;
 
     private List<Debuff> debuffs = new List<Debuff>();
 
     [SerializeField]
     private int silverBoltStacks = 0;
 
+    private float tempSpeed;
+
+
     //private Vector3 startingPosition;
 
     private void Start()
     {
+        //Physics2D.IgnoreLayerCollision();
         health = maxHealth;
         player = GameObject.FindWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
@@ -46,12 +57,12 @@ public class BasicEnemyAI : MonoBehaviour, IEnemy
         target = GameObject.FindWithTag("Player").transform;
         int targetHealth = target.GetComponent<Health>().health;
         col = GetComponent<Collider2D>();
+        tempSpeed = speed * (float)0.1;
     }
 
     void Update()
     {
-        //isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
-
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
 
         dir = target.position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
@@ -79,25 +90,24 @@ public class BasicEnemyAI : MonoBehaviour, IEnemy
             Physics2D.IgnoreCollision(xp.GetComponent<Collider2D>(), col);
         }
         */
+
         HandleDebuffs();
 
 
         MoveCharacter(movement);
-        /*
-        if (!isInAttackRange)
-        {
-            MoveCharacter(movement);
-        }
-        else        
-        {
-            rb.velocity = Vector2.zero;
-        }
-        */
     }
 
     private void MoveCharacter(Vector2 dir)
     {
-        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+        if (isInAttackRange)
+        {
+            rb.MovePosition((Vector2)transform.position + (dir * tempSpeed * Time.deltaTime));
+            Shoot();
+        }
+        else
+        {
+            rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+        }
     }
 
     public void ContactDamage(int targetHealth)
@@ -125,7 +135,7 @@ public class BasicEnemyAI : MonoBehaviour, IEnemy
 
     private void HandleDebuffs()
     {
-        Debug.Log(silverBoltStacks);
+        //Debug.Log(silverBoltStacks);
 
         var tempList = debuffs;
         //int count = 0;
@@ -141,9 +151,27 @@ public class BasicEnemyAI : MonoBehaviour, IEnemy
         }
 
         if (silverBoltStacks == 3)
-        {                        
+        {
             TakeDamage(maxHealth * (float)0.12);
-            silverBoltStacks = 0;                      
+            silverBoltStacks = 0;
+        }
+    }
+
+    public void Shoot()
+    {
+        Vector2 playerPosition = player.transform.position;
+        playerPosition.Normalize();
+
+        if (Time.time > lastShootTime + fireRate)
+        {
+
+            GameObject bullet = Instantiate(bulletPrefab, rb.position, Quaternion.identity);
+            Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
+
+            rbBullet.AddForce(dir * bulletSpeed, ForceMode2D.Impulse);
+
+            lastShootTime = Time.time;
         }
     }
 }
+
